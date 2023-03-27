ebfa7d98a481a714c4b6888e61a752779a3a1634
chulbul <chulbul.singh@awign.com>
2023-03-27 10:39:17 +0530

    filter by status is working fine

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+import Task from './components/Task'
+import {BiSearchAlt} from 'react-icons/bi';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  // search value stored
+  const [searchTask, setSearchTask] = useState("");
+
+  return (
+    <>
+      <div className="App">
+        <div className="header">
+          Task Manager App
+          <div className="search">
+            <input
+              type="text"
+              name=""
+              id=""
+              onChange={(e) => {
+                setSearchTask(e.target.value);
+              }}
+              placeholder="search ..."
+              maxLength={50}
+            />
+            <label htmlFor="">
+              <BiSearchAlt className="search_icon" />
+            </label>
+          </div>
+        </div>
+        <Task searchTask={searchTask} />
+      </div>
+      <Toaster position="bottom-left" />
+    </>
+  );
+}
+
+export default App;
